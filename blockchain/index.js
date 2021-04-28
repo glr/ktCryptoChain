f57009ee98dee095c75c1dfae4af0280ce8d4c4a
Gabriel Rael <glrael@gmail.com>
2021-04-27 22:11:44 -0400

    chain sync between peers

--- blockchain/index.js
+++ blockchain/index.js
@@ -12,8 +12,6 @@ const pubsub = new PubSub({blockchain});
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
-
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -47,5 +45,7 @@ if (process.env.GENERATE_PEER_PORT) {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
-    syncChains();
+    if (PORT !== DEFAULT_PORT) {
+        syncChains();
+    }
 });
