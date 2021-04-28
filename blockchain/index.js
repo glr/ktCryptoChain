6d128768ad2cab4566de91069d1f2b102a15d37c
Gabriel Rael <glrael@gmail.com>
2021-04-27 22:02:38 -0400

    fix typo bug

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const request = require('request')
 const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain.js')
 const PubSub = require('../app/pubsub')
+const { response } = require('express')
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -22,11 +27,25 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on a sync with ', rootChain);
+            blockchain.replaceChain(rootChain);
+        } else {
+            console.error(`ERROR: unable to replace chain on a sync`, error)
+        }
+    })
+}
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT) {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+    syncChains();
+});
