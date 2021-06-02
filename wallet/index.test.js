b0a4aa94bcea9416b82e3a715cb87a9cdd80d9d9
Gabriel Rael <glrael@gmail.com>
2021-06-01 21:01:51 -0400

    add ability to create a transaction from the wallet

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require('./index');
-const { verifySignature } = require('../util/index')
+const { verifySignature } = require('../util/index');
+const Transaction = require('./transaction');
 
 describe ('Wallet', () => {
     let wallet;
@@ -40,5 +41,36 @@ describe ('Wallet', () => {
         });
 
 
+    });
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws an error', () => {
+                expect(() => wallet.createTransaction({amount: 99999999, recipient: 'foo-recipient'}))
+                    .toThrow('Amount exceeds balance');
+            });
+        });
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient
+
+            beforeEach (() => {
+                amount = 50;
+                recipient = 'foo-recipient',
+                transaction = wallet.createTransaction({amount, recipient})
+            });
+
+            it('creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            });
+
+            it('matches the transaction input with the wallet', () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey);
+            });
+
+            it('outputs the amount to the recipient', () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount);
+            });
+        });
     });
 });
